cdbfd3f0ef78a7b10c04b5ebed71c158045f55cf
tjdesigner <tiagodejesus.design@gmail.com>
2021-08-27 01:04:19 -0300

    feat: add imagens, diretórios e configuração do header

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title> Atividade Mapa</title>
-  </head>
-  <body></body>
+
+<?php
+  include('pages/header.php')
+?>
+
+
+  </body>
 </html>
---
+++ pages/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaisei+HarunoUmi:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500;600;700&display=swap" rel="stylesheet">
+    <title> Atividade Mapa</title>
+  </head>
+  <body>
+    <header class="header">
+        <div class=header-content>
+            <div class="logo">
+                <img src="images/logo.jpeg">
+            </div>
+            <div class="menu">
+                <ul >
+                    <li class="menu-item active">
+                        <a href="#">Quem somos</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Contato</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Localização</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
