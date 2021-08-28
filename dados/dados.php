8ca4659d6fc5a6a16d242adfffaf5dd3564e3ad1
tjdesigner <tiagodejesus.design@gmail.com>
2021-08-27 23:10:23 -0300

    feat: páginas detalhes e ajustes de layout e dados

--- dados/dados.php
+++ dados/dados.php
@@ -3,41 +3,43 @@
     $quem_somos_c1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.";
     $quem_somos_c2 = "But also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
     
+    $descricao = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.";
+
     $produtos = array(
         0 => array(
             'img' => 'images/brigadeiro.jpeg',
             'titulo' => 'Brigadeiro',
-            'descricao' => 'Descrição asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',
+            'descricao' => $descricao,
             'link' => 'brigadeiro',
         ),
         1 => array(
             'img' => 'images/cocada.jpeg',
             'titulo' => 'Cocada',
-            'descricao' => 'Descrição asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',
+            'descricao' => $descricao,
             'link' => 'cocada',
         ),
         2 => array(
             'img' => 'images/doce_abobora.jpeg',
             'titulo' => 'Doce de abóbora',
-            'descricao' => 'Descrição asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',
+            'descricao' => $descricao,
             'link' => 'doce_abobora',
         ),
         3 => array(
             'img' => 'images/doce_leite.jpeg',
             'titulo' => 'Doce de leite',
-            'descricao' => 'Descrição asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',
+            'descricao' => $descricao,
             'link' => 'doce_leite',
         ),
         4 => array(
             'img' => 'images/pe_moleque.jpeg',
             'titulo' => 'Pé de moleque',
-            'descricao' => 'Descrição asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',
+            'descricao' => $descricao,
             'link' => 'pe_moleque',
         ),
         5 => array(
             'img' => 'images/torta_gelada.jpeg',
             'titulo' => 'Torta gelada',
-            'descricao' => 'Descrição asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',
+            'descricao' => $descricao,
             'link' => 'torta_gelada',
         ),        
     );
--- pages/localizacao.php
+++ pages/localizacao.php
@@ -1,1 +1,14 @@
-<div>LOCALIZAÇÃO</div>
+<?php 
+    include('dados/dados.php')
+?>
+
+<div class="page-container">
+    <div class="container content">
+        <div class="mt-5">
+            <h2>Encotre a melhor rota</h2>
+        </div>
+        <div class="row mt-5">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491462.06469303026!2d-48.077301418323785!3d-15.775065601550303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a3d18e45b91a3%3A0x24e8d3620bd85d7f!2sBras%C3%ADlia%20-%20DF!5e0!3m2!1spt-BR!2sbr!4v1630116090560!5m2!1spt-BR!2sbr" width="900" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+</div>
